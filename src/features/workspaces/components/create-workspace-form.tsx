058f2c06914f2e689453b5917b1f39d624fcaf55
Caetano <caetano.bona@gmail.com>
2025-04-09 15:11:15 -0300

    added image attribute to workspace w/ image registering functionality

--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import { z } from "zod";
+import { useRef } from "react";
 import { useForm } from "react-hook-form";
+import Image from "next/image";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+import { Avatar, AvatarFallback } from "@radix-ui/react-avatar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Form,
@@ -19,6 +22,7 @@ import { DottedSeparator } from "@/components/dotted-separator";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useCreateWorkspace } from "../api/use-create-workspace.ts";
+import { ImageIcon } from "lucide-react";
 
 interface CreateWorkspaceFormProps {
   onCancel? : () => void;
@@ -26,6 +30,8 @@ interface CreateWorkspaceFormProps {
 
 export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
   const { mutate, isPending } = useCreateWorkspace();
+
+  const inputRef = useRef<HTMLInputElement>(null)
   
   const form = useForm<z.infer<typeof createWorkspaceSchema>>({
     resolver : zodResolver(createWorkspaceSchema),
@@ -35,7 +41,19 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
   });
 
   const onSubmit = (values : z.infer<typeof createWorkspaceSchema>) => {
-    mutate({ json : values });
+    const finalValues = {
+      ...values,
+      image: values.image instanceof File ? values.image : "" // zodValidator(createWorkspaceSchema) will treat "" as undefined
+    }
+
+    mutate({ form: finalValues })
+  }
+
+  const handleImageChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]; // getting the file from the onChange event
+    if (file) {
+      form.setValue("image", file) // setting the new image as the value for the "image" input
+    }
   }
 
   return (
@@ -68,6 +86,60 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
                     </FormControl>
                   </FormItem>
                 )}      
+              />
+              <FormField 
+                control={form.control}
+                name="image"
+                render={({ field }) => (
+                  <div className="flex flex-col gap-y-2">
+                    <div className="flex items-center gap-x-5">
+                      {field.value ? ( // if the user added an image, show it
+                        <div className="size-[72px] relative rounded-md overflow-hidden">
+                          <Image 
+                            alt="Logo"
+                            fill
+                            className="object-cover"
+                            src={
+                              field.value instanceof File
+                                ? URL.createObjectURL(field.value) // if the image is a file, display it (through ObjectURL)
+                                : field.value // if the image is not a file, show the value (probably an URL)
+                            }
+                          />
+                        </div>
+                      ) : ( // if the user didnt add an image, show an image icon
+                        <Avatar className="size-[72px]">
+                          <AvatarFallback>
+                            <ImageIcon className="size-[36px] text-neutral-400"/>
+                          </AvatarFallback>
+                        </Avatar>
+                      )}
+                      <div className="flex flex-col">
+                        <p className="text-sm">Workspace Icon</p>
+                        <p className="text-sm text-muted-foreground">
+                          JPG, PNG, SVG or JPEG, max 1mb
+                        </p>
+                        <input // hidden native input to hold the value
+                          className="hidden"
+                          type="file"
+                          accept=".jpg, .png, .jpeg, .svg"
+                          ref={inputRef}
+                          onChange={handleImageChange}
+                          disabled={isPending}
+                        />
+                        <Button // Button to call the hidden native input above
+                          type="button"
+                          disabled={isPending}
+                          variant="teritary"
+                          size="xs"
+                          className="w-fit mt-2"
+                          onClick={() => inputRef.current?.click()}
+                        >
+                          Upload Image
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                )}
               />
             </div>
             <DottedSeparator className="py-7"/>
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -4,19 +4,37 @@ import { zValidator } from "@hono/zod-validator";
 import { sessionMiddleware } from "@/lib/session-middleware";
 
 import { createWorkspaceSchema } from "../schemas";
-import { DATABASE_ID, WORKSPACE_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACE_ID } from "@/config";
 import { ID } from "node-appwrite";
 
 const app = new Hono()
   .post(
     "/",
-    zValidator("json", createWorkspaceSchema),
+    zValidator("form", createWorkspaceSchema),
     sessionMiddleware,
     async (c) => {
       const databases = c.get("databases");
+      const storage = c.get("storage") // getting the storage (defined in middleware)
       const user = c.get("user");
 
-      const { name, image } = c.req.valid("json");
+      const { name, image } = c.req.valid("form");
+
+      let uploadedImageUrl : string | undefined; // undefined in case user didn't upload image
+
+      if (image instanceof File) { // if user uploaded a file for image
+        const file = await storage.createFile( // creating the File for bucket
+            IMAGES_BUCKET_ID,
+            ID.unique(),
+            image
+        );
+        
+        const arrayBuffer = await storage.getFilePreview( // getting the ArrayBuffer for the file created
+          IMAGES_BUCKET_ID,
+          file.$id,
+        );
+
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}` // transforming ArrayBuffer to an URL
+      }
 
       const workspace = await databases.createDocument(
         DATABASE_ID,
@@ -25,6 +43,7 @@ const app = new Hono()
         {
           name,
           userId : user.$id,
+          imageUrl : uploadedImageUrl // adding the image to the workspace document
         },
       );
 
